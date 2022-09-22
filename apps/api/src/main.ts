ea5f43c57ade96df77234b521a0879b358654637
micahgoodreau0912 <micahgoodreau@gmail.com>
2022-09-22 01:55:19 -0400

    build graphql api

---
+++ apps/api/src/main.ts
@@ -0,0 +1,26 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/graphiql`
+  );
+}
+
+bootstrap();
